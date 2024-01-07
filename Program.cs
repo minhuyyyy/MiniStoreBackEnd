0c360be93b781d26b920638f3aef1a95e74d5fa5
Trần Thanh Minh Huy <113049694+minhuyyyy@users.noreply.github.com>
2024-01-07 09:43:45 +0700

    fix connectionstr

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MiniStoreContext>(o =>
 {
-    o.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL CONNECTIONSTRING"));
+    o.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
 });
 
 builder.Services.AddCors(options =>
